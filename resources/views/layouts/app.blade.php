b0ca1f8454818b9cc95fa0a5893c75a574273524
abcdefghijkok <aaa111sssbbbb@gmail.com>
2024-10-21 23:22:49 +0900

    update:body最後に移動

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,6 @@
     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
      <link rel="shortcut icon" href="{{ asset('/images/logo.ico') }}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -34,5 +31,7 @@
         </main>
         
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
