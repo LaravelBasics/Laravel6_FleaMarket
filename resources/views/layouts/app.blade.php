55dc984f57dff51895f7411a15e2cfd6fca5383f
abcdefghijkok <aaa111sssbbbb@gmail.com>
2024-10-22 20:54:37 +0900

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,20 @@
             
             @yield('content')
         </main>
-        
+        <?php
+if (extension_loaded('gd')) {
+    echo "GDライブラリは有効です。\n";
+    $gdInfo = gd_info();
+    echo "GD Version: " . $gdInfo['GD Version'] . "\n";
+    echo "FreeType Support: " . ($gdInfo['FreeType Support'] ? 'はい' : 'いいえ') . "\n";
+    echo "JPEG Support: " . ($gdInfo['JPEG Support'] ? 'はい' : 'いいえ') . "\n";
+    echo "PNG Support: " . ($gdInfo['PNG Support'] ? 'はい' : 'いいえ') . "\n";
+    echo "GIF Support: " . ($gdInfo['GIF Support'] ? 'はい' : 'いいえ') . "\n";
+} else {
+    echo "GDライブラリは有効ではありません。\n";
+}
+?>
+
     </div>
 
     <!-- jQuery -->
